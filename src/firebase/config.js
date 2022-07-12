52f8ca8424c2902a5b9758c67a9fe91da2b20994
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-07-12 11:11:02 +0400

    starting

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCIJIp3GOlZrezVxaRe1Dq10zFjqRz_jGU",
+  authDomain: "udemi-chat.firebaseapp.com",
+  projectId: "udemi-chat",
+  storageBucket: "udemi-chat.appspot.com",
+  messagingSenderId: "24431176421",
+  appId: "1:24431176421:web:326a7069b519b42aa1420c",
+  measurementId: "G-LJ5JZYCD8T"
+};
+
+
+//init firebase
+
+firebase.initializeApp(firebaseConfig)
+
+const projectFirestore = firebase.firestore()
+const timestamp = firebase.firestore.FieldValue.serverTimestamp
+
+
+export { projectFirestore, timestamp}
