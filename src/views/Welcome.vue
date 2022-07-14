360ef565a4c0e51d5b830d3bc58e12af061973f7
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-07-14 10:39:45 +0400

    signup login

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,11 +1,32 @@
 <template>
   <div class="welcome container">
     <p>Welcome</p>
+    <div v-if="ShowLogin">
+      <h2>Log in</h2>
+      <LoginForm />
+      <p>No account yet?<span @click="ShowLogin = false">Signup</span> instead</p>
+    </div>
+    <div v-else>
+      <h2>Sign up</h2>
+      <SignupForm />
+      <p>Already registered?<span @click="ShowLogin = true">Login</span> instead</p>
+    </div>
   </div>
 </template>
 
 <script>
+import SignupForm from '../components/SignupForm.vue'
+import LoginForm from '../components/LoginForm.vue'
+import { ref } from 'vue'
+
 export default {
+  components:{ SignupForm, LoginForm },
+
+  setup(){
+    const ShowLogin = ref(true)
+
+    return{ ShowLogin}
+  }
 
 }
 </script>
@@ -14,5 +35,28 @@ export default {
 .welcome{
     text-align: center;
     padding: 20px 0;
+}
+
+.welcome label{
+  display: block;
+  margin: 20px auto;
+}
+.welcome input {
+  display: block;
+  width: 30%;
+  padding: 10px;
+  border-radius: 20px;
+  border: 1px solid #eee;
+  outline: none;
+  color: #999;
+  margin: 10px auto;
+}
+.welcome span{
+  font-weight: bold;
+  text-decoration: underline;
+  cursor: pointer;
+}
+.welcome button{
+  margin: 20px auto;
 }
 </style>
