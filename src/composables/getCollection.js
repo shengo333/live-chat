01ec0c9ccafebcf66cbab33b7ea99859c8b66990
shengo333 <BEKASHENGELIA333@GMAIL.COM>
2022-07-21 11:00:43 +0400

    finished

--- src/composables/getCollection.js
+++ src/composables/getCollection.js
@@ -1,5 +1,5 @@
-import { ref } from 'vue';
-import { projectFirestore } from '../firebase/config';
+import { ref, watchEffect } from 'vue'
+import { projectFirestore } from '../firebase/config'
 
 const getCollection = (collection) => {
 
@@ -10,8 +10,9 @@ const getCollection = (collection) => {
   let collectionRef = projectFirestore.collection(collection)
     .orderBy('createdAt')
 
-  collectionRef.onSnapshot(snap => {
-    let results = [];
+  const unsub = collectionRef.onSnapshot(snap => {
+    console.log(snap)
+    let results = []
     snap.docs.forEach(doc => {
       // must wait for the server to create the timestamp & send it back
       // we don't want to edit data until it has done this
@@ -19,13 +20,18 @@ const getCollection = (collection) => {
     });
     
     // update values
-    documents.value = results;
-    error.value = null;
+    documents.value = results
+    error.value = null
   }, err => {
     console.log(err.message)
     documents.value = null
     error.value = 'could not fetch the data'
   })
+
+  watchEffect((onInvalidate) => {
+    // unsub from prev collection when watcher is stopped (component unmounted)
+    onInvalidate(() => unsub());
+  });
 
   return { error, documents }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Welcome from '../views/Welcome.vue'
 import Chatroom from '../views/Chatroom.vue'
-import { projectAuth } from '@/firebase/config'
+import { projectAuth } from '../firebase/config'
 
-
-//auth guard
-
-const requireAuth = (to, from, next) =>{
+// auth guards
+const requireAuth = (to, from, next) => {
   let user = projectAuth.currentUser
   console.log('current user in auth guard: ', user)
-  if(!user){
-    next({name: 'Welcome'})
-  } else{
+  if (!user) {
+    next({ name: 'Welcome' })
+  } else {
     next()
   }
 }
 
+const requireNoAuth = (to, from, next) => {
+  let user = projectAuth.currentUser
+  if (user) {
+    next({ name: 'Chatroom' })
+  } else {
+    next()
+  }
+}
 
 const routes = [
   {
     path: '/',
     name: 'Welcome',
-    component: Welcome
+    component: Welcome,
+    beforeEnter: requireNoAuth
   },
   {
     path: '/chatroom',
